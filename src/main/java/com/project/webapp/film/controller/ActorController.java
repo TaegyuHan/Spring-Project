77e4a721442e17196556fa5ee90489b29e3c9757
Taegyu <59700057+TaegyuHan@users.noreply.github.com>
2023-05-04 17:37:25 +0900

    수정 : @Controller > @RestController 어노테이션 수정

--- src/main/java/com/project/webapp/film/controller/ActorController.java
+++ src/main/java/com/project/webapp/film/controller/ActorController.java
@@ -11,11 +11,10 @@ import jakarta.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
-@Controller
+@RestController
 @RequestMapping(ApiVersionController.URL_API_VERSION)
 public class ActorController {
     
